c1a74a020b6f6f392e6a1b4f4ea806624ec769bd
Mirella <mirellafarben@gmail.com>
2021-01-29 23:41:04 -0300

    Update automated_whatsapp_Alpha_0.2.py
    Most of the things that were in the main function have been taken off, 'because the main app will be the same one that creates the GUI

--- automated_whatsapp_Alpha_0.2.py
+++ automated_whatsapp_Alpha_0.2.py
@@ -8,28 +8,32 @@ import tabula
 
 
 def whatsapp_newline_translator(texto_pra_formatar):
-    mensagem_inicial = open (texto_pra_formatar, 'r', encoding='utf8', errors='ignore')
 
-    codigo = '%0' + 'a'
-    text = ""
-    lines = mensagem_inicial.readlines ()
-    for line in lines:
-        text += line.replace ("\n", ('%0' + 'a'))
+    if isinstance(texto_pra_formatar, str):
+
+        text = texto_pra_formatar.replace("\n", ('%0' + 'a'))
+
+    else:
+        mensagem_inicial = ""
+        mensagem_inicial += open (texto_pra_formatar, 'r', encoding='utf-8', errors='ignore')
+        text = ""
+        lines = mensagem_inicial.readlines ()
+        for line in lines:
+            text += line.replace ("\n", ('%0' + 'a'))
 
     return text
 
 
 def csv_translator(planilha):
-
-    if str(planilha).endswith('.pdf'):
+    if str (planilha).endswith ('.pdf'):
         tabula.convert_into (planilha, "output.csv", output_format='csv', pages='all')
-    elif str(planilha).endswith('.xlsx'):
+    elif str (planilha).endswith ('.xlsx'):
         df = pandas.read_excel (planilha)
         df.to_csv ("output.csv", sep=",")
-    elif str(planilha).endswith('.csv'):
+    elif str (planilha).endswith ('.csv'):
         pass
     else:
-        print("Arquivo de planilha não suportado.")
+        print ("Arquivo de planilha não suportado.")
 
 
 def message_sender(planilha, telefone, message):
@@ -47,11 +51,11 @@ def message_sender(planilha, telefone, message):
         wb.open ('https://web.whatsapp.com/send?phone=' + final_number + '&text=' + message)
         width, height = pg.size ()
         pg.click (width / 2, height / 2)
-        time.sleep (20)
+        time.sleep (10)
 
         pg.press ('enter')
 
-        time.sleep (2)
+        time.sleep (1)
         pg.hotkey ('ctrl', 'w')
         pg.press ('enter')
 
@@ -62,29 +66,13 @@ def message_sender(planilha, telefone, message):
 
 
 def main():
-
     telefones = ["TELEFONE1", "TELEFONE2", "TELEFONE3"]
-    root = tk.Tk ()
-    root.withdraw ()
-
-    print ("Selecione o documento de texto com a mensagem que você quer enviar. ")
-    time.sleep (3)
-    texto_pra_formatar = filedialog.askopenfilename ()
-    mensagem = whatsapp_newline_translator (texto_pra_formatar)
-    message = str (mensagem)
-
-
-    time.sleep (2)
-    print ("Pronto, agora... Selecione a planilha onde os números estão.")
-    time.sleep (2)
-    planilha_com_numeros = filedialog.askopenfilename ()
-    csv_translator(planilha_com_numeros)
 
     print ("iniciando o processo...")
 
     for telefone in telefones:
-        message_sender('output.csv', telefone, message)
+        message_sender ('output.csv', telefone, message)
 
 
 if __name__ == '__main__':
-    main()
+    main ()
